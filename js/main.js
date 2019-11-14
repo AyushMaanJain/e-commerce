4660f98bcfddf45ceaeb296b1bf33cd7199adedf
AyushMaanJain <34471203+AyushMaanJain@users.noreply.github.com>
2019-11-14 19:35:11 +0530

    search bar for mobile done
    still some work on responsiveness remain

--- js/main.js
+++ js/main.js
@@ -14,7 +14,6 @@ var searchBoxMaargins=0;
          $("#sidenav").width(0);
          openSlider=false;
     }
-    
     var width=window.innerWidth;
   var height=window.innerHeight;
   searchBoxMaargins=(width-768)/4;
@@ -37,7 +36,23 @@ var searchBoxMaargins=0;
          openSide(300);
      }
   });
+   
+    function searchOn(){
+      $(".on-search").css("display","none");
+      $(".collapsed-search").css("display","block");
+    }
+    function searchOff(){
+      $(".on-search").css("display","block");
+      $(".collapsed-search").css("display","none");
+    }
   
+  $(".search-toggle").click(function(){
+  searchOn();    
+  });
+  $("#rev").click(function(){
+  searchOff();    
+  });
+
     $(window).resize(function(){
   width=window.innerWidth;
   height=window.innerHeight; 
@@ -50,8 +65,6 @@ var searchBoxMaargins=0;
     if(width<768){
         closeSide();
     }
-    
-    //cardcontainer
     var conw=$(".card-container").width();
     var count=(conw%216)/4;
     $(".fix-card").html(count+"px");
