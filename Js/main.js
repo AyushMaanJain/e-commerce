65be0f692c1be534f04d95ae9b9f4269bfcd114b
AyushMaanJain <34471203+AyushMaanJain@users.noreply.github.com>
2019-11-21 13:55:38 +0530

    Update main.js

--- Js/main.js
+++ Js/main.js
@@ -8,17 +8,9 @@ $(document).ready(function(){
     $(".options").css("opacity","0");
   });
   $(".slideToggle").focus(function(){
-     $(".slidenav").css("width","250px"); 
+     $(".slidenav").attr("expand","true"); 
   });
   $(".slideToggle").focusout(function(){
-     $(".slidenav").css("width","0px"); 
+    $(".slidenav").attr("expand","false");
   });
-  function resize(){
-      var w=window.innerWidth;
-      if(w>768){
-          $(".popup").css("display","block");
-          $(".slideToggle").css("display","block");
-           $(".slidenav").css("width","250px"); 
-      }
-  }
 });
