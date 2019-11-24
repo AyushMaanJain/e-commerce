331f916f574185ad990ad497bf9d24cf97d75fe4
AyushMaanJain <34471203+AyushMaanJain@users.noreply.github.com>
2019-11-24 11:04:49 +0530

    Update main.js

--- Js/main.js
+++ Js/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    var slide=1;
+    var w=window.innerWidth;
+    
   $(".popup").focus(function(){
     $(".options").attr("display","block");
   });
@@ -19,14 +22,86 @@ $(document).ready(function(){
       $(".searchCon").attr("display","none");
   });
   
+
+ if(w>768){
+      $(".home").css("width",(w-250)+"px");
+      $(".carousel_container").css("height",(w-300)/3+"px");
+        }
+else if(w<=768){
+      $(".home").css("width",(w+0)+"px");
+      $(".carousel_container").css("height",(w-100)/2+"px");  
+      }
+  
+  $("#shiftL").click(function(){
+  if(slide===1){
+      
+      $("#textA").attr("active","false");
+      $("#textC").attr("active","true");
+      $("#picA").attr("active","false");
+      $("#picC").attr("active","true");
+      $(".desc").css("background","#00f");
+      slide=3;
+  // alert(slide);
+     } else
+  if(slide===3){
+      $("#textC").attr("active","false");
+      $("#textB").attr("active","true");
+      $("#picC").attr("active","false");
+      $("#picB").attr("active","true");
+      $(".desc").css("background","#000");
+       slide=2;
+      //alert(slide);
+     } else
+  if(slide===2){
+      $("#textB").attr("active","false");
+      $("#textA").attr("active","true");
+      $("#picB").attr("active","false");
+      $("#picA").attr("active","true");
+      $(".desc").css("background","#207");
+      slide=1;
+      //alert(slide);
+     } 
+  });
+  $("#shiftR").click(function(){
+  if(slide===1){
+      $("#textA").attr("active","false");
+      $("#textB").attr("active","true");
+      $("#picA").attr("active","false");
+      $("#picB").attr("active","true");
+      $(".desc").css("background","#000");
+      slide=2;
+  // alert(slide);
+     } else
+  if(slide===3){
+      $("#textC").attr("active","false");
+      $("#textA").attr("active","true");
+      $("#picC").attr("active","false");
+      $("#picA").attr("active","true");
+      $(".desc").css("background","#207");
+      slide=1;
+      //alert(slide);
+     } else
+  if(slide===2){
+      $("#textB").attr("active","false");
+      $("#textC").attr("active","true");
+      $("#picB").attr("active","false");
+      $("#picC").attr("active","true");
+      $(".desc").css("background","#00f");
+      slide=3;
+      //alert(slide);
+     } 
+  });
+  
   $(window).resize(function(){
       var w=window.innerWidth;
       if(w>768){
       $(".home").css("width",(w-250)+"px");
-        }else if(w<=768){
-      $(".home").css("width","100%");
+      $(".carousel_container").css("height",(w-300)/3+"px");
         }
+        else if(w<=768){
+      $(".home").css("width",(w+0)+"px");
+      $(".carousel_container").css("height",(w-100)/2+"px");  
+      }
       
   });
-  
 });
