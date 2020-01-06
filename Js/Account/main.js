7c60607a936f3b016a3dfa334985cd3041f75841
AyushMaanJain <34471203+AyushMaanJain@users.noreply.github.com>
2020-01-06 09:53:13 +0530

    update with fixed bugs

--- Js/Account/main.js
+++ Js/Account/main.js
@@ -1,129 +1,188 @@
 $(document).ready(function(){
-    var w=window.innerWidth;
-    var marginvar=w/5;
-    var marginfix=30+40+15-80;
-     $(".body").css("padding-top",marginvar+marginfix+"px");
-    if(w>768){
-        var bodywidth=w-185;
-     $(".body").css("width",bodywidth+"px");
-     $(".part").css("width",bodywidth-30+"px");
-     if(w>992){
-     var halfFormWidth=(bodywidth-30)/2;
-     var halfFormInputWidth=halfFormWidth-90;
-     $(".half-input").css("width",halfFormInputWidth);
-        }else
-     if(w<992){
-     var halfFormWidth=(bodywidth-30);
-     var halfFormInputWidth=halfFormWidth-90;
-     $(".half-input").css("width",halfFormInputWidth);
-        }
- }
-    else if(w<768){
-     var bodywidth=w-15;
-     $(".body").css("width",bodywidth+"px");
-     $(".part").css("width",bodywidth-10+"px");
-      var halfFormWidth=(bodywidth-30);
-     var halfFormInputWidth=halfFormWidth-65;
-     $(".half-input").css("width",halfFormInputWidth);
- }
+     var url=window.location.href;
+    if(url.toString().includes("?user")){
+        $("#cart").css("display","none");
+        $("#tran").css("display","none");
+        $("#user").css("display","block");
+    }else
+    if(url.toString().includes("?cart")){
+        $("#cart").css("display","block");
+        $("#tran").css("display","none");
+        $("#user").css("display","none");
+    }else
+    if(url.toString().includes("?transection")){
+        $("#cart").css("display","none");
+        $("#tran").css("display","block");
+        $("#user").css("display","none");
+    }
+    
+    var w=window.innerWidth; 
+    var minSize=0;
+    var maxSize=0;
+    var minMargin=2;
+    var maxMargin=0;
+    var textMargins=0;
+    var bodywidth=0;
+    var bodypadding=0;
+    var halfFormSize=0;
+    var paymentWay=0;
+if(w>992){
+    bodypadding=20/100*w;
+    bodywidth=w-160;
+    halfFormSize=(bodywidth-40)/2;
+    minSize=8;
+    maxSize=20;
+    maxMargin=40;
+    textMargins=6/100*w;
+    paymentWay=40+30;
+    $("img").css("width","20vw");
+    $("img").css("height","20vw");
+    $("img").css("margin-left","40vw");
+    $("img").css("margin-right","40vw");
+    
+    $(".uname").css("margin-top",-10+"px");
+}else
+if(w>768){
+    bodypadding=25/100*w;
+    bodywidth=w-160;
+    minSize=10;
+    maxSize=25;
+    maxMargin=37.5;
+    textMargins=8/100*w;
+    halfFormSize=(bodywidth-40);
+    paymentWay=40+30;
+    $("img").css("width","25vw");
+    $("img").css("height","25vw");
+    $("img").css("margin-left","37.5vw");
+    $("img").css("margin-right","37.5vw");
+    
+     $(".uname").css("margin-top",-10+"px");
+}else
+if(w<=768){
+    bodypadding=30/100*w;
+    bodywidth=w;
+    minSize=13;
+    maxSize=30;
+    maxMargin=35;
+    textMargins=11/100*w;
+    halfFormSize=bodywidth-40;
+    paymentWay=30;
+    $("img").css("width","30vw");
+    $("img").css("height","30vw");
+    $("img").css("margin-left","35vw");
+    $("img").css("margin-right","35vw");
+    $("[width=auto]").css("width",(bodywidth-paymentWay-30)+"px");
+    $(".uname").css("margin-top",-10+"px");
+    
+}
+
+{$(".body").css("width",bodywidth+"px");
+$(".body").css("padding-top",bodypadding+10+"px");
+$(".part").css("width",bodywidth-40+"px");
+$(".prod-name").css("width",(bodywidth-40-20)*2/3+"px");
+$(".product-name").css("width",(bodywidth-40-20)*2/4+"px");
+$(".prod-price").css("width",(bodywidth-40-20)*1/3+"px");
+$(".product-date").css("width",(bodywidth-40-20)*1/4+"px");
+$(".product-bill").css("width",(bodywidth-40-20)*1/4+"px");
+$(".payment-way").css("width",(bodywidth-paymentWay)+"px");
+$(".half-input").css("width",halfFormSize-40-55+"px");}
 
 
 $(window).scroll(function(){
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-
-  var Margin=(winScroll/5.263);
-  var maxMargin=40;
-  var Top=(winScroll/2.857);
-  
-  var Size=(winScroll/13.33);
-  var minSize=5;
-  
-  if(w<992){
-  Size=(winScroll/20);
-  minSize=10;
-  //bodyTop=-60;
-  Top=(winScroll/3.63);
-  }
-  var maxSize=20;
-if(Margin<38){
-$(".uname").css("margin-top",(-1*Top)+"px");
-$("img").css("margin-left",(maxMargin-Margin)+"vw");
-$("img").css("margin-right",(maxMargin-Margin)+"vw");
-$("img").css("width",(maxSize-Size)+"vw");
-$("img").css("height",(maxSize-Size)+"vw");
-
-}else
-if(Margin>38){
-    $("img").css("margin-left",2+"vw");
-    $("img").css("margin-right",2+"vw");
-  if(w<992){    
-    $("img").css("width",10+"vw");
-    $("img").css("height",10+"vw");
-    $(".uname").css("margin-top",-60+"px");
-  }else if(w>992){
-    $("img").css("width",6+"vw");
-    $("img").css("height",6+"vw");
-    $(".uname").css("margin-top",-70+"px");
-            }}
-    var scrollVal=1;
-if(winScroll>=1){
-    scrollVal=1;
-}
-if(winScroll>=790){
-    scrollVal=2;
-}
-if(winScroll>=1150){
-    scrollVal=3;
-}
-if(winScroll>=1320){
-    scrollVal=4;
-}
-if(winScroll>=1930){
-    scrollVal=5;
-}
-if(winScroll>=2100){
-    scrollVal=6;
-}
-//$(".uname").html("|"+winScroll+"|");
-for(var i=1;i<=6;i++){
-    if(scrollVal===i){
-        $("#li"+i).attr("state","active");
+    //scroll data
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var scrollLimit=200;
+    //range
+    var SizeRange=maxSize-minSize;
+    var marginRange=maxMargin-minMargin;
+    var textRange=textMargins;
+    //function
+    var ScrollSize=winScroll/scrollLimit*SizeRange;
+    var ScrollMargin=winScroll/scrollLimit*marginRange;
+    var ScrollTextMargin=winScroll/scrollLimit*textRange;
+    //final variable
+    var sizeOnScroll=maxSize-ScrollSize;
+    var marginOnScroll=maxMargin-ScrollMargin;
+    
+    
+    
+    if(winScroll<200){
+        $("img").css("width",sizeOnScroll+"vw");
+        $("img").css("height",sizeOnScroll+"vw");
+        $("img").css("margin-left",marginOnScroll+"vw");
+        $("img").css("margin-right",marginOnScroll+"vw");
+        $(".uname").css("margin-top",-10-ScrollTextMargin+"px");
     }
-    else{
-    $("#li"+i).attr("state","inactive");
-}
-}
-  }
-          );
-
-$(window).resize(function(){
-          w=window.innerWidth;
-    var marginvar=w/5;
-    var marginfix=30+40+15-80;
-     $(".body").css("padding-top",marginvar+marginfix+"px");
+    if(winScroll>=200){
+        $("img").css("width",minSize+"vw");
+        $("img").css("height",minSize+"vw");
+        $("img").css("margin-left",minMargin+"vw");
+        $("img").css("margin-right",minMargin+"vw");
+        $(".uname").css("margin-top",-10-textRange+"px");
+    }
      
-        if(w>768){
-        var bodywidth=w-185;
-     $(".body").css("width",bodywidth+"px");
-     $(".part").css("width",bodywidth-30+"px");
-     if(w>992){
-     var halfFormWidth=(bodywidth-30)/2;
-     var halfFormInputWidth=halfFormWidth-90;
-     $(".half-input").css("width",halfFormInputWidth);
-        }else
-     if(w<992){
-     var halfFormWidth=(bodywidth-30);
-     var halfFormInputWidth=halfFormWidth-90;
-     $(".half-input").css("width",halfFormInputWidth);
-        }
- }
-        else if(w<768){
-     var bodywidth=w-15;
-     $(".body").css("width",bodywidth+"px");
-     $(".part").css("width",bodywidth-10+"px");
-     var halfFormWidth=(bodywidth-30);
-     var halfFormInputWidth=halfFormWidth-65;
-     $(".half-input").css("width",halfFormInputWidth);
- }
-     });
- });
+});
+$(window).resize(function(){
+   w=window.innerWidth;
+if(w>992){
+    bodypadding=20/100*w;
+    bodywidth=w-160;
+    halfFormSize=(bodywidth-40)/2;
+    minSize=8;
+    maxSize=20;
+    maxMargin=40;
+    textMargins=6/100*w;
+    paymentWay=40+30;
+    $("img").css("width","20vw");
+    $("img").css("height","20vw");
+    $("img").css("margin-left","40vw");
+    $("img").css("margin-right","40vw");
+    
+    $(".uname").css("margin-top",-10+"px");
+}else
+if(w>768){
+    bodypadding=25/100*w;
+    bodywidth=w-160;
+    minSize=10;
+    maxSize=25;
+    maxMargin=37.5;
+    textMargins=8/100*w;
+    halfFormSize=(bodywidth-40);
+    paymentWay=40+30;
+    $("img").css("width","25vw");
+    $("img").css("height","25vw");
+    $("img").css("margin-left","37.5vw");
+    $("img").css("margin-right","37.5vw");
+    
+     $(".uname").css("margin-top",-10+"px");
+}else
+if(w<=768){
+    bodypadding=30/100*w;
+    bodywidth=w;
+    minSize=13;
+    maxSize=30;
+    maxMargin=35;
+    textMargins=11/100*w;
+    halfFormSize=bodywidth-40;
+    paymentWay=30;
+    $("img").css("width","30vw");
+    $("img").css("height","30vw");
+    $("img").css("margin-left","35vw");
+    $("img").css("margin-right","35vw");
+    $("[width=auto]").css("width",(bodywidth-paymentWay-30)+"px");
+    $(".uname").css("margin-top",-10+"px");
+    
+}
+
+{$(".body").css("width",bodywidth+"px");
+$(".body").css("padding-top",bodypadding+10+"px");
+$(".part").css("width",bodywidth-40+"px");
+$(".prod-name").css("width",bodywidth-40-40-120+"px");
+$(".product-name").css("width",bodywidth-40-40-320+"px");
+$(".prod-price").css("width",100+"px");
+$(".product-date").css("width",200+"px");
+$(".product-bill").css("width",100+"px");
+$(".payment-way").css("width",(bodywidth-paymentWay)+"px");
+$(".half-input").css("width",halfFormSize-40-55+"px");}
+});
+});
