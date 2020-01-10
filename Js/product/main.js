b267768dcc95ed20e3d84dacf7d71f6e545df38d
AyushMaanJain <34471203+AyushMaanJain@users.noreply.github.com>
2020-01-10 11:56:36 +0530

    product page updated

---
+++ Js/product/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    var w=window.innerWidth;
+    var headerHeight=70;
+    
+    if(w>900){
+    $(".head").css("margin-left",(w-900)/2+"px");}
+    else if(w<900){
+    $(".head").css("margin-left",0+"px");
+if(w<544){
+    headerHeight=0;
+}
+}
+
+    var header=document.getElementsByClassName("header");
+    var firstHead=header[0].offsetTop;
+$(".body").css("padding-top",firstHead+200+"px");
+
+    $(window).scroll(function(){
+    var scrolled=window.pageYOffset;
+    var scrollForHead=scrolled/firstHead;
+    var scrollForHeadSize=70-(scrolled-firstHead);
+    if (scrolled > firstHead) {
+    $(".header").addClass("fix-head");
+    $(".header").css("background","rgba(153,51,204,1)");
+    if(scrolled < (firstHead+headerHeight)){
+        $(".fixed-container").css("margin-top",25+scrollForHeadSize+"px");   
+    }else{
+       $(".fixed-container").css("margin-top",25+"px");
+    }
+  } else {
+    $(".fixed-container").css("margin-top",25+headerHeight+"px");
+    $(".header").removeClass("fix-head");
+    $(".header").css("background","rgba(153,51,204,"+scrollForHead+")");
+    
+  }
+  
+});
+
+    $(window).resize(function(){
+       w=window.innerWidth;  
+        if(w>900){
+    $(".head").css("margin-left",(w-900)/2+"px");}
+        else if(w<900){
+    $(".head").css("margin-left",0+"px");
+if(w<544){
+    headerHeight=0;
+}
+}
+
+    header=document.getElementsByClassName("header");
+    firstHead=header[0].offsetTop;
+    });
+});
